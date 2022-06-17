55665888d352ba87e7bfae615a0b738d14d5b25c
nathaly <nathy_peretto@hotmail.com>
2022-06-17 14:28:02 -0300

    Atualizado

---
+++ Chapter1/Contexts/ChapterContext.cs
@@ -0,0 +1,35 @@
+﻿
+using Chapter1.Models;
+using Chapter1.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter1.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+        }
+        // vamos utilizar esse metodo para configurar o banco de dados
+        protected override void
+
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+
+                optionsBuilder.UseSqlServer("Data Source = NATHALY\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
---
+++ Chapter1/Program.cs
@@ -0,0 +1,73 @@
+using Chapter1.Contexts;
+using Chapter1.Interfaces;
+using Chapter1.Repositories;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors( options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyHeader();
+    });
+});
+
+//padrão do token
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultChallengeScheme = "JwtBerer";
+    options.DefaultAuthenticateScheme = "JwtBerer";
+}).AddJwtBearer("JwtBerer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("chapter-chave-autenticacao")),
+        ClockSkew = TimeSpan.FromMinutes(60),
+        ValidIssuer = "chapter.webapi",
+        ValidAudience = "chapter.webapi"
+    };
+});
+
+builder.Services.AddScoped<ChapterContext, ChapterContext>();
+
+builder.Services.AddTransient<LivroRepository, LivroRepository>();
+
+builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
